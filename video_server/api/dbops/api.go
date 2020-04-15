8c98bab7d40f2067d2ffebcb7185b4d0b188909e
biaogexf <897040621@qq.com>
2020-04-15 11:04:14 +0800

    api之数据库层实现——实现User

--- video_server/api/dbops/api.go
+++ video_server/api/dbops/api.go
@@ -3,15 +3,45 @@ package dbops
 import (
 	_ "database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"log"
 )
 
 
 func AddUserCredential(loginName string, pwd string) error {
+	stmtIns, err := dbConn.Prepare("INSERT INTO users (login_name, pwd) VALUES (?, ?)")
+	if err != nil {
+		return err
+	}
 
+	stmtIns.Exec(loginName, pwd)
+	stmtIns.Close()
+	return nil
 }
 
 func GetUserCredential(loginName string) (string, error) {
+	stmtOut, err := dbConn.Prepare("SELECT pwd FROM users WHERE login_name = ?")
+	if err != nil {
+		log.Printf("%s", err)
+		return "", err
+	}
 
+	var pwd string
+	stmtOut.QueryRow(loginName).Scan(&pwd)
+	stmtOut.Close()
+
+	return pwd, nil
+}
+
+func DeleteUser(loginName string, pwd string) error {
+	stmtDel, err := dbConn.Prepare("DELETE FROM users WHERE login_name = ? AND pwd = ?")
+	if err != nil {
+		log.Printf("DeleteUser error: %s", err)
+		return err
+	}
+
+	stmtDel.Exec(loginName, pwd)
+	stmtDel.Close()
+	return nil
 }
 
 /*
--- video_server/api/dbops/conn.go
+++ video_server/api/dbops/conn.go
@@ -10,6 +10,7 @@ var (
 	err error
 )
 
+// 只要调用dbops包，则该函数自动执行
 func init() {
 	dbConn, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/video_server?charset=utf8")
 	if err != nil {
