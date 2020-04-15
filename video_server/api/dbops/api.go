34c1faddede824420f6e2c404b73124453666c89
biaogexf <897040621@qq.com>
2020-04-15 16:22:30 +0800

    api之数据库层实现——实现和验证Video

--- video_server/api/dbops/api.go
+++ video_server/api/dbops/api.go
@@ -1,13 +1,15 @@
 package dbops
 
 import (
+	"../defs"
+	"../utils"
 	"database/sql"
-	_ "database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"time"
 )
 
-
+// User
 func AddUserCredential(loginName string, pwd string) error {
 	stmtIns, err := dbConn.Prepare("INSERT INTO users (login_name, pwd) VALUES (?, ?)")
 	if err != nil {
@@ -52,6 +54,73 @@ func DeleteUser(loginName string, pwd string) error {
 	defer stmtDel.Close()
 	return nil
 }
+
+// Video
+func AddNewVideo(aid int, name string) (*defs.VideoInfo, error) {
+	// create uuid
+	vid, err := utils.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+
+	t := time.Now()
+	ctime := t.Format("Jan 02 2006, 15:04:05") // M D Y, HH:MM:SS
+	stmtIns, err := dbConn.Prepare(`INSERT INTO video_info
+    (id, author_id, name, display_ctime) VALUES(?, ?, ?, ?)`)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = stmtIns.Exec(vid, aid, name, ctime)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &defs.VideoInfo{Id: vid, AuthorId: aid, Name: name, DisplayCtime:ctime}
+
+	defer stmtIns.Close()
+	return res, nil
+}
+
+func GetVideoInfo(vid string) (*defs.VideoInfo, error) {
+	// create uuid
+	stmtOut, err := dbConn.Prepare("SELECT author_id, name, display_ctime FROM video_info WHERE id=?")
+
+	var aid int
+	var dct string
+	var name string
+
+	err = stmtOut.QueryRow(vid).Scan(&aid, &name, &dct)
+	if err != nil && err != sql.ErrNoRows {
+		return nil, err
+	}
+
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+
+	defer stmtOut.Close()
+
+	res := &defs.VideoInfo{Id:vid, AuthorId:aid, Name:name, DisplayCtime:dct}
+	return res, nil
+}
+
+func DeleteVideoInfo(vid string) error {
+	stmtDel, err := dbConn.Prepare("DELETE FROM video_info WHERE id = ?")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmtDel.Exec(vid)
+	if err != nil {
+		return err
+	}
+
+	defer stmtDel.Close()
+	return nil
+}
+
+
 
 /*
 1. api的操作主要是对DB的CRUD
--- video_server/api/defs/apidef.go
+++ video_server/api/defs/apidef.go
@@ -5,3 +5,11 @@ type UserCredential struct {
 	Username string `json:"username"`
 	Pwd string `json:"pwd"`
 }
+
+// Data model
+type VideoInfo struct {
+	Id string `json:"id"`
+	AuthorId int `json:"author_id"`
+	Name string `json:"name"`
+	DisplayCtime string `json:"display_ctime"`
+}
