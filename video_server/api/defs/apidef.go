38cb0a163b9fdc4b9d8725356891dcf1264e3a05
biaogexf <897040621@qq.com>
2020-04-16 16:42:46 +0800

    完成后端api模块

--- video_server/api/defs/apidef.go
+++ video_server/api/defs/apidef.go
@@ -6,13 +6,46 @@ type UserCredential struct {
 	Pwd string `json:"pwd"`
 }
 
+type NewVideo struct {
+	AuthorId int `json:"author_id"`
+	Name string `json:"name"`
+}
+
+type NewComment struct {
+	AuthorId int `json:"author_id"`
+	Content string `json:"content"`
+}
+
 // response
 type SignedUp struct {
 	Success bool `json:"success"`
 	SessionId string `json:"session_id"`
 }
 
+type SignedIn struct {
+	Success bool `json:"success"`
+	SessionId string `json:"session_id"`
+}
+
+type VideosInfo struct {
+	Videos []*VideoInfo `json:"videos"`
+}
+
+type UserInfo struct {
+	Id int `json:"id"`
+}
+
+type Comments struct {
+	Comments []*Comment `json:"comments"`
+}
+
 // Data model
+type User struct {
+	Id int
+	LoginName string
+	Pwd string
+}
+
 type VideoInfo struct {
 	Id string `json:"id"`
 	AuthorId int `json:"author_id"`
--- video_server/api/main.go
+++ video_server/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"./session"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
@@ -21,22 +22,31 @@ func (m middleWareHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	m.r.ServeHTTP(w, r)
 }
 
-
 func RegisterHandlers() *httprouter.Router {
 	router := httprouter.New()
 
 	router.POST("/user", CreateUser)
 	router.POST("/user/:username", Login)
+	router.GET("/user/:username", GetUserInfo)
+	router.POST("/user/:username/videos", AddNewVideo)
+	router.GET("/user/:username/videos", ListAllVideos)
+	router.DELETE("/user/:username/videos/:vid-id", DeleteVideo)
+	router.POST("/videos/:vid-id/comments", PostComment)
+	router.GET("/videos/:vid-id/comments", ShowComments)
 
 	return router
 }
 
+func Prepare() {
+	session.LoadSessionsFromDB()
+}
+
 func main() {
+	Prepare()
 	r := RegisterHandlers()
 	mh := NewMiddleWareHandler(r)
 	http.ListenAndServe(":8000", mh)
 }
-
 /*
 listen->RegisterHandlers->handlers
 1. go语言对于每一个handler都用一个go routine来处理（默认）
