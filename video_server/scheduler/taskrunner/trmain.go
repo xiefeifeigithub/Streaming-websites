67c90247bab4e7e4d299befb5c09ca9fb9715a67
biaogexf <897040621@qq.com>
2020-04-21 14:51:59 +0800

    timer的实现

--- video_server/scheduler/taskrunner/trmain.go
+++ video_server/scheduler/taskrunner/trmain.go
@@ -1,1 +1,44 @@
 package taskrunner
+
+import "time"
+
+/*
+timer
+setup
+start:{trigger->task->runner}
+Worker为定时器，Runner为任务
+将定时器绑定到任务上！！！
+ */
+
+type Worker struct {
+	ticker *time.Ticker
+	runner *Runner
+}
+
+func NewWorker(interval time.Duration, r *Runner) *Worker {
+	return &Worker{
+		ticker: time.NewTicker(interval * time.Second),
+		runner: r,
+	}
+}
+
+func (w *Worker) startWorker() {
+	for {
+		select {
+		case <-w.ticker.C:
+			go w.runner.StartAll()
+		}
+	}
+}
+
+func Start() {
+	// Start video file cleaning
+	r := NewRunner(3,true, VideoClearDispatcher, VideoClearExecutor)
+	w := NewWorker(3, r)
+	go w.startWorker()
+
+	//// Something else
+	//r1
+	//w1
+	//go w1
+}
