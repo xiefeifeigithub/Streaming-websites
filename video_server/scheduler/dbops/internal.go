d60741fc5c8c5ef65ee6d239e36592d0fe11a318
biaogexf <897040621@qq.com>
2020-04-21 11:40:26 +0800

    task示例的实现

--- video_server/scheduler/dbops/internal.go
+++ video_server/scheduler/dbops/internal.go
@@ -1,1 +1,51 @@
 package dbops
+
+import "log"
+
+/*
+1.api->video_id-mysql
+2.dispatcher->mysql-video_id->datachannel
+3.executor->datachannel-video_id->delete videos
+*/
+
+func ReadVideoDeletionRecord(count int) ([]string, error) {
+	stmtOut, err := dbConn.Prepare("SELECT video_id FROM video_del_rec LIMIT ?")
+	var ids []string
+	if err != nil {
+		return ids, err
+	}
+
+	rows, err := stmtOut.Query(count)
+	if err != nil {
+		log.Printf("Query VideoDeletionRecord err: %v", err)
+		return ids, err
+	}
+
+	for rows.Next() {
+		var id string
+		if err := rows.Scan(&id); err != nil {
+			return ids, err
+		}
+
+		ids = append(ids, id)
+	}
+
+	defer stmtOut.Close()
+	return ids, nil
+}
+
+func DelVideoDeletionRecord(vid string) error {
+	stmtDel, err := dbConn.Prepare("DELETE FROM video_del_rec WHERE video_id = ?")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmtDel.Exec(vid)
+	if err != nil {
+		log.Printf("Deleting VideoDeletionRecord: %v", err)
+		return err
+	}
+
+	defer stmtDel.Close()
+	return nil
+}
--- video_server/scheduler/taskrunner/defs.go
+++ video_server/scheduler/taskrunner/defs.go
@@ -4,6 +4,8 @@ const (
 	READY_TO_DISPATCH = "d"
 	READY_TO_EXECUTE = "e"
 	CLOSE = "c"
+
+	VIDEO_PATH = "../../streamserver/videos/"
 )
 
 type controlChan chan string
