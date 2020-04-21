84951e6ac1944ee4b06628b746caa2f115de9bf7
biaogexf <897040621@qq.com>
2020-04-21 15:34:06 +0800

    api实现以及scheduler完成

--- video_server/scheduler/dbops/api.go
+++ video_server/scheduler/dbops/api.go
@@ -1,1 +1,27 @@
 package dbops
+
+import "log"
+
+/*
+Scheduler服务的业务逻辑
+1. user -> api service ->delete video
+2. api server -> scheduler -> write video deletion record
+3. timer
+4. timer -> runner -> read wvdr -> exec -> delete video from folder
+ */
+
+func AddVideoDeletionRecord(vid string) error {
+	stmtIns, err := dbConn.Prepare("INSERT INTO video_del_rec (video_id) VALUES (?)")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmtIns.Exec(vid)
+	if err != nil {
+		log.Printf("AddVideoDeletionRecord error: %v", err)
+		return err
+	}
+
+	defer stmtIns.Close()
+	return nil
+}
--- video_server/scheduler/handlers.go
+++ video_server/scheduler/handlers.go
@@ -1,1 +1,25 @@
-package scheduler
+package main
+
+import (
+	"./dbops"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func vidDelRecHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	vid := p.ByName("vid-id")
+
+	if len(vid) == 0 {
+		sendResponse(w, 400, "Video id should not be empty")
+		return
+	}
+
+	err := dbops.AddVideoDeletionRecord(vid)
+	if err != nil {
+		sendResponse(w, 500, "Internal server error")
+		return
+	}
+
+	sendResponse(w, 200, "deletion successful")
+	return
+}
